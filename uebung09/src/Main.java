6a6e2a84dd5716c13b7105d3cea3f0feed7bdd6f
Timo <Timo@T.Speedport_W_921V_1_39_000>
2015-12-16 12:15:38 +0100

    added Beispielsatz

--- uebung09/src/Main.java
+++ uebung09/src/Main.java
@@ -1,16 +1,17 @@
 
 public class Main
 {
+    
+    public static void main(String[] args)
+    {
+        new Main();
+    }
 
-	public static void main(String[] args)
-	{
-		new Main();
-	}
-
-	public Main()
-	{
-		HMM hmm = new HMM("hdt-1-10000-train.tags");
-		Viterbi viterbi = new Viterbi(hmm);
-		viterbi.mostLikelySequence("Silicon");
-	}
+    public Main()
+    {
+        HMM hmm = new HMM("hdt-1-10000-train.tags");
+        
+        Viterbi viterbi = new Viterbi(hmm);
+        viterbi.mostLikelySequence("Das ist einfach nur ein kleiner Beispielsatz");
+    }
 }
