6e767e342b13582137db93e1ac7d1c7dad3ff914
Timo <Timo@T.Speedport_W_921V_1_39_000>
2015-12-16 12:14:20 +0100

    Try to get tag of words[i]

--- uebung09/src/Viterbi.java
+++ uebung09/src/Viterbi.java
@@ -1,28 +1,35 @@
+import java.util.HashMap;
 
 public class Viterbi
 {
-	private HMM _hmm;
-	
-	public Viterbi(HMM hmm)
-	{
-		_hmm = hmm;
-	}
-	
-	// TODO Satz taggen
-	public String mostLikelySequence(String sentence)
-	{
-		String[] words = sentence.split("\\s+");
-		StringBuffer buffer = new StringBuffer();
-		String tag;
-		
-		for(int i = 0; i < words.length; i++)
-		{
-			// TODO get tag of words[i]
-			tag = "";
-			
-			buffer.append(words[i] + " " + tag);
-		}
-		
-		return buffer.toString();
-	}
+    private HMM _hmm;
+    private HashMap<String,Integer> start_word = new HashMap<String,Integer>();
+    
+    public Viterbi(HMM hmm)
+    {
+        _hmm = hmm;
+    }
+    
+    // TODO Satz taggen
+    public String mostLikelySequence(String sentence)
+    {
+        String[] words = sentence.split("\\s+");
+        StringBuffer buffer = new StringBuffer();
+        String tag="";
+        
+        
+        for(int i = 0; i < words.length; i++)
+        {
+            // TODO get tag of words[i]
+            Word word= new Word(words[i]);
+           
+            Tag ta = new Tag(tag);
+            word.addTag(ta);
+            tag = word.getTags().toString();
+            buffer.append(words[i] + " " + tag);
+            System.out.println(words[i]  + " " + tag);
+        }
+        
+        return buffer.toString();
+    }
 }
