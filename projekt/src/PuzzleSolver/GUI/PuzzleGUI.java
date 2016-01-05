1dcb1493e0abeddfa9445bc4693babbf20d8a8a5
theSoenke <theSoenke@users.noreply.github.com>
2016-01-05 21:56:10 +0100

    Package angepasst

--- projekt/src/PuzzleSolver/GUI/PuzzleGUI.java
+++ projekt/src/PuzzleSolver/GUI/PuzzleGUI.java
@@ -1,4 +1,4 @@
-package GUI;
+package PuzzleSolver.GUI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -17,155 +17,159 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
-
 public class PuzzleGUI
 {
-    private JPanel mainpanel;
-    private JButton solve;
-    private JButton reset;
-    private JFrame frame;
-    private GraphicsPanel puzzleGraphics;
-    private PuzzleWerkzeug werkzeug = new PuzzleWerkzeug();
+	private JPanel mainpanel;
+	private JButton solve;
+	private JButton reset;
+	private JFrame frame;
+	private GraphicsPanel puzzleGraphics;
+	private PuzzleWerkzeug werkzeug = new PuzzleWerkzeug();
 
-    public PuzzleGUI()
-    {
-        initGUI();
-    }
+	public PuzzleGUI()
+	{
+		initGUI();
+	}
 
-    private void implementButtonPanel()
-    {
-        mainpanel = new JPanel();
-        GridLayout main = new GridLayout();
-        mainpanel.setLayout(main);
-        frame.getContentPane()
-            .add(mainpanel, BorderLayout.SOUTH);
-        mainpanel.setBackground(new Color(255, 255, 255));
-        implementSolveButton();
-        implementResetButton();
-    }
+	private void implementButtonPanel()
+	{
+		mainpanel = new JPanel();
+		GridLayout main = new GridLayout();
+		mainpanel.setLayout(main);
+		frame.getContentPane().add(mainpanel, BorderLayout.SOUTH);
+		mainpanel.setBackground(new Color(255, 255, 255));
+		implementSolveButton();
+		implementResetButton();
+	}
 
-    private void implementPuzzle()
-    {
-        puzzleGraphics = new GraphicsPanel();
-        frame.getContentPane()
-            .add(puzzleGraphics, BorderLayout.CENTER);
-    }
+	private void implementPuzzle()
+	{
+		puzzleGraphics = new GraphicsPanel();
+		frame.getContentPane().add(puzzleGraphics, BorderLayout.CENTER);
+	}
 
-    private void initGUI()
-    {
-        frame = new JFrame("15-Puzzle");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.getContentPane()
-            .setBackground(new Color(255, 255, 255));
-        frame.setTitle("15-Puzzle");
-        implementButtonPanel();
-        implementPuzzle();
-        frame.pack();
-        frame.setSize(337, 409);
-    }
+	private void initGUI()
+	{
+		frame = new JFrame("15-Puzzle");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.setTitle("15-Puzzle");
+		implementButtonPanel();
+		implementPuzzle();
+		frame.pack();
+		frame.setSize(337, 409);
+	}
 
-    private void implementSolveButton()
-    {
-        solve = new JButton();
-        mainpanel.add(solve);
-        solve.setText("Solve!");
-        solve.setPreferredSize(new java.awt.Dimension(180, 50));
-        solve.setFont(new Font("Tahoma", Font.BOLD, 14));
-        solve.setBackground(new Color(200, 200, 200));
-        solve.setSelected(true);
-    }
+	private void implementSolveButton()
+	{
+		solve = new JButton();
+		mainpanel.add(solve);
+		solve.setText("Solve!");
+		solve.setPreferredSize(new java.awt.Dimension(180, 50));
+		solve.setFont(new Font("Tahoma", Font.BOLD, 14));
+		solve.setBackground(new Color(200, 200, 200));
+		solve.setSelected(true);
+	}
 
-    private void implementResetButton()
-    {
-        reset = new JButton();
-        mainpanel.add(reset);
-        reset.setText("Reset");
-        reset.setPreferredSize(new java.awt.Dimension(180, 50));
-        reset.setFont(new Font("Tahoma", Font.BOLD, 14));
-        reset.setBackground(new Color(200, 200, 200));
-        reset.setSelected(true);
-        reset.addActionListener(new NewGameAction());
-    }
+	private void implementResetButton()
+	{
+		reset = new JButton();
+		mainpanel.add(reset);
+		reset.setText("Reset");
+		reset.setPreferredSize(new java.awt.Dimension(180, 50));
+		reset.setFont(new Font("Tahoma", Font.BOLD, 14));
+		reset.setBackground(new Color(200, 200, 200));
+		reset.setSelected(true);
+		reset.addActionListener(new NewGameAction());
+	}
 
-    public void zeigeFenster()
-    {
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
+	public void showUI()
+	{
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 
-    class GraphicsPanel extends JPanel implements MouseListener
-    {
-        private static final int ROWS = 4;
-        private static final int COLS = 4;
-        private static final int CELL_SIZE = 80; // Pixels
-        private Font _biggerFont;
-        String number;
+	class GraphicsPanel extends JPanel implements MouseListener
+	{
+		private static final long serialVersionUID = 497341847642354581L;
+		private static final int ROWS = 4;
+		private static final int COLS = 4;
+		private static final int CELL_SIZE = 80; // Pixels
+		private Font _biggerFont;
+		String number;
 
-        public GraphicsPanel()
-        {
-            _biggerFont = new Font("SansSerif", Font.BOLD, CELL_SIZE / 2);
-            this.setPreferredSize(
-                    new Dimension(CELL_SIZE * COLS, CELL_SIZE * ROWS));
-            this.setBackground(Color.black);
-            this.addMouseListener(this);
-        }
+		public GraphicsPanel()
+		{
+			_biggerFont = new Font("SansSerif", Font.BOLD, CELL_SIZE / 2);
+			this.setPreferredSize(new Dimension(CELL_SIZE * COLS, CELL_SIZE * ROWS));
+			this.setBackground(Color.black);
+			this.addMouseListener(this);
+		}
 
-        public void paintComponent(Graphics g)
-        {
-            super.paintComponent(g);
-            for (int r = 0; r < ROWS; r++)
-            {
-                for (int c = 0; c < COLS; c++)
-                {
-                    int x = c * CELL_SIZE;
-                    int y = r * CELL_SIZE;
-                    String text = werkzeug.getNumber(r, c);
-                    if (text != null)
-                    {
-                        g.setColor(Color.gray);
-                        g.fillRect(x + 2, y + 2, CELL_SIZE - 4, CELL_SIZE - 4);
-                        g.setColor(Color.black);
-                        g.setFont(_biggerFont);
-                        g.drawString(text, x + 20, y + (3 * CELL_SIZE) / 4);
-                    }
-                }
-            }
-        }
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+			for (int r = 0; r < ROWS; r++)
+			{
+				for (int c = 0; c < COLS; c++)
+				{
+					int x = c * CELL_SIZE;
+					int y = r * CELL_SIZE;
+					String text = werkzeug.getNumber(r, c);
+					if (text != null)
+					{
+						g.setColor(Color.gray);
+						g.fillRect(x + 2, y + 2, CELL_SIZE - 4, CELL_SIZE - 4);
+						g.setColor(Color.black);
+						g.setFont(_biggerFont);
+						g.drawString(text, x + 20, y + (3 * CELL_SIZE) / 4);
+					}
+				}
+			}
+		}
 
-        @Override
-        public void mouseClicked(MouseEvent arg0){}
+		@Override
+		public void mouseClicked(MouseEvent arg0)
+		{
+		}
 
-        @Override
-        public void mouseEntered(MouseEvent arg0){}
+		@Override
+		public void mouseEntered(MouseEvent arg0)
+		{
+		}
 
-        @Override
-        public void mouseExited(MouseEvent arg0){}
+		@Override
+		public void mouseExited(MouseEvent arg0)
+		{
+		}
 
-        @Override
-        public void mousePressed(MouseEvent e)
-        {
-            int col = e.getX()/CELL_SIZE;
-            int row = e.getY()/CELL_SIZE;
-            
-            if (!werkzeug.moveTile(row, col))
-            {
-                Toolkit.getDefaultToolkit().beep();
-            }
-            
-            this.repaint();
-        }
+		@Override
+		public void mousePressed(MouseEvent e)
+		{
+			int col = e.getX() / CELL_SIZE;
+			int row = e.getY() / CELL_SIZE;
 
-        @Override
-        public void mouseReleased(MouseEvent arg0){}
-    }
+			if (!werkzeug.moveTile(row, col))
+			{
+				Toolkit.getDefaultToolkit().beep();
+			}
 
-    public class NewGameAction implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e)
-        {
-            werkzeug.reset();
-            puzzleGraphics.repaint();
-        }
-    }
+			this.repaint();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0)
+		{
+		}
+	}
+
+	public class NewGameAction implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			werkzeug.reset();
+			puzzleGraphics.repaint();
+		}
+	}
 
 }
--- projekt/src/PuzzleSolver/GUI/PuzzleWerkzeug.java
+++ projekt/src/PuzzleSolver/GUI/PuzzleWerkzeug.java
@@ -1,117 +1,119 @@
-package GUI;
+package PuzzleSolver.GUI;
 
 public class PuzzleWerkzeug
 {
-    private static final int ROWS = 4;
-    private static final int COLS = 4;
+	private static final int ROWS = 4;
+	private static final int COLS = 4;
 
-    private Tile[][] contents;
-    private Tile emptyTile;
+	private Tile[][] contents;
+	private Tile emptyTile;
 
-    public PuzzleWerkzeug()
-    {
-        contents = new Tile[ROWS][COLS];
-        reset();
-    }
+	public PuzzleWerkzeug()
+	{
+		contents = new Tile[ROWS][COLS];
+		reset();
+	}
 
-    String getNumber(int row, int col)
-    {
-        return contents[row][col].getNumber();
-    }
+	String getNumber(int row, int col)
+	{
+		return contents[row][col].getNumber();
+	}
 
-    public void reset()
-    {
-        for (int r = 0; r < ROWS; r++)
-        {
-            for (int c = 0; c < COLS; c++)
-            {
-                contents[r][c] = new Tile(r, c, "" + (r * COLS + c + 1));
-            }
-        }
+	public void reset()
+	{
+		for (int r = 0; r < ROWS; r++)
+		{
+			for (int c = 0; c < COLS; c++)
+			{
+				contents[r][c] = new Tile(r, c, "" + (r * COLS + c + 1));
+			}
+		}
 
-        emptyTile = contents[ROWS - 1][COLS - 1];
-        emptyTile.setNumber(null);
+		emptyTile = contents[ROWS - 1][COLS - 1];
+		emptyTile.setNumber(null);
 
-        // Tausche Tile mit einer einer zufälligen Tile
-        for (int r = 0; r < ROWS; r++)
-        {
-            for (int c = 0; c < COLS; c++)
-            {
-                switchTiles(r, c, (int) (Math.random() * ROWS),
-                        (int) (Math.random() * COLS));
-            }
-        }
-    }
+		// Tausche Tile mit einer einer zufälligen Tile
+		for (int r = 0; r < ROWS; r++)
+		{
+			for (int c = 0; c < COLS; c++)
+			{
+				switchTiles(r, c, (int) (Math.random() * ROWS), (int) (Math.random() * COLS));
+			}
+		}
+	}
 
-    private void switchTiles(int r1, int c1, int r2, int c2)
-    {
-        Tile temp = contents[r1][c1];
-        contents[r1][c1] = contents[r2][c2];
-        contents[r2][c2] = temp;
-    }
-    
-    public boolean moveTile(int r, int c) 
-    {
-        return checkEmptyTile(r, c, -1, 0) || checkEmptyTile(r, c, 1, 0)
-            || checkEmptyTile(r, c, 0, -1) || checkEmptyTile(r, c, 0, 1);
-    }
-    
-    private boolean checkEmptyTile(int r, int c, int rdelta, int cdelta) 
-    {
-        int rNeighbor = r + rdelta;
-        int cNeighbor = c + cdelta;
-        //--- Check to see if this neighbor is on board and is empty.
-        if (isLegal(rNeighbor, cNeighbor) 
-                  && contents[rNeighbor][cNeighbor] == emptyTile) {
-            switchTiles(r, c, rNeighbor, cNeighbor);
-            return true;
-        }
-        return false;
-    }
-    
-    public boolean isLegal(int r, int c) 
-    {
-        return r>=0 && r<ROWS && c>=0 && c<COLS;
-    }
-    
-    public boolean isGameOver() {
-        for (int r=0; r<ROWS; r++) {
-            for (int c=0; c<ROWS; c++) {
-                Tile t = contents[r][c];
-                return t.isInFinalPosition(r, c);
-            }
-        }
-        return true;
-    }
+	private void switchTiles(int r1, int c1, int r2, int c2)
+	{
+		Tile temp = contents[r1][c1];
+		contents[r1][c1] = contents[r2][c2];
+		contents[r2][c2] = temp;
+	}
+
+	public boolean moveTile(int r, int c)
+	{
+		return checkEmptyTile(r, c, -1, 0) || checkEmptyTile(r, c, 1, 0) || checkEmptyTile(r, c, 0, -1)
+				|| checkEmptyTile(r, c, 0, 1);
+	}
+
+	private boolean checkEmptyTile(int r, int c, int rdelta, int cdelta)
+	{
+		int rNeighbor = r + rdelta;
+		int cNeighbor = c + cdelta;
+		// --- Check to see if this neighbor is on board and is empty.
+		if (isLegal(rNeighbor, cNeighbor) && contents[rNeighbor][cNeighbor] == emptyTile)
+		{
+			switchTiles(r, c, rNeighbor, cNeighbor);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isLegal(int r, int c)
+	{
+		return r >= 0 && r < ROWS && c >= 0 && c < COLS;
+	}
+
+	public boolean isGameOver()
+	{
+		for (int r = 0; r < ROWS; r++)
+		{
+			for (int c = 0; c < ROWS; c++)
+			{
+				Tile t = contents[r][c];
+				return t.isInFinalPosition(r, c);
+			}
+		}
+		return true;
+	}
 
 }
 
 class Tile
 {
 
-    private int row;
-    private int col;
-    private String number;
+	private int row;
+	private int col;
+	private String number;
 
-    public Tile(int r, int c, String n)
-    {
-        row = r;
-        col = c;
-        number = n;
-    }
+	public Tile(int r, int c, String n)
+	{
+		row = r;
+		col = c;
+		number = n;
+	}
 
-    public void setNumber(String newNumber)
-    {
-        number = newNumber;
-    }
+	public void setNumber(String newNumber)
+	{
+		number = newNumber;
+	}
 
-    public String getNumber()
-    {
-        return number;
-    }
-    
-    public boolean isInFinalPosition(int r, int c)
-    {
-        return r==row && c==col;
-    }
+	public String getNumber()
+	{
+		return number;
+	}
+
+	public boolean isInFinalPosition(int r, int c)
+	{
+		return r == row && c == col;
+	}
 }
